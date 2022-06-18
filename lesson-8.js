93e52de54350c01fcee736b3d071be02947a6827
dmgame <denis.m.pcspace@gmail.com>
2022-06-18 17:07:50 +0300

    Add additional tasks

--- lesson-8.js
+++ lesson-8.js
@@ -134,3 +134,26 @@ const getSortedCarsOnSale = cars => {
 // console.log(getSortedCarsOnSale(cars));
 
 // some, every
+
+
+// Додаткові завдання
+/**
+ * 1. Создать функции:
+- первая функция принимает массив и колбэк (одна для всех вызовов)
+- Под каждый пример (свой callback) который обрабатывает каждый элемент массива
+
+Первая функция возвращает строку “New value: ” и результат работы callback:
+
+Примеры:
+firstFunc([‘my’, ‘name’, ‘is’, ‘Trinity’], handler1) → “New value: MyNameIsTrinity”
+firstFunc([10, 20, 30], handler2) → “New value: 100, 200, 300,”
+firstFunc([{age: 45, name: ‘Jhon’}, {age: 20, name: ‘Aaron’}], handler3) →“New value: Jhon is 45, Aaron is 20,”
+firstFunc([‘abc’, ‘123’], handler4) → “New value: cba, 321,” // строки инвертируются
+*/
+
+/**
+ * 2. Создать функции аналогичные перебирающим методм массивов, которые будут полностью повторять их поведение.
+ * Например метод every, findIndex, map, reduce и тд
+ * Главное условие не использовать данные методы внутри вашей реализации аналогов.
+*/
+
